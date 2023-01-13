8e4c93b0935006881c3171e409e89f452d241c14
midcondria <hyukkind@naver.com>
2023-01-13 23:53:12 +0900

    basic

---
+++ src/main/java/shop/iamhyunjun/ostargram/domain/post/dto/PostUpdateDto.java
@@ -0,0 +1,18 @@
+package my.board.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class PostUpdateDto {
+
+    @NotNull
+    private String author;
+
+    @NotBlank
+    private String title;
+
+    private String content;
+}
