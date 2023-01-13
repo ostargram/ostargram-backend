a8cdd9d1c98b87648f9f4a677e4d48a63b8af03a
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-01-13 15:45:29 +0900

    Initial commit

---
+++ src/main/java/shop/iamhyunjun/ostargram/OstargramApplication.java
@@ -0,0 +1,13 @@
+package shop.iamhyunjun.ostargram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OstargramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OstargramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shop/iamhyunjun/ostargram/OstargramApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.iamhyunjun.ostargram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OstargramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
