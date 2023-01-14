e1bcee6c4ad720680260fecdc92a2d9764254554
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-01-14 22:56:03 +0900

    Test Class삭제

--- src/main/java/shop/iamhyunjun/ostargram/domain/user/entity/TestRepository.java
+++
@@ -1,6 +0,0 @@
-package shop.iamhyunjun.ostargram.domain.user.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
-}
