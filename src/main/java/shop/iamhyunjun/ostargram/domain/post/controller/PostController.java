d0458c613414ce9b6359bc355ad500cb6f772649
midcondria <hyukkind@naver.com>
2023-01-14 15:58:54 +0900

    글 작성 조회 기능 초기버전

--- src/main/java/shop/iamhyunjun/ostargram/domain/post/controller/PostController.java
+++ src/main/java/shop/iamhyunjun/ostargram/domain/post/controller/PostController.java
@@ -1,4 +1,77 @@
 package shop.iamhyunjun.ostargram.domain.post.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import shop.iamhyunjun.ostargram.domain.post.dto.PostResponseDto;
+import shop.iamhyunjun.ostargram.domain.post.dto.PostSaveDto;
+import shop.iamhyunjun.ostargram.domain.post.dto.PostUpdateDto;
+import shop.iamhyunjun.ostargram.domain.post.entity.Post;
+import shop.iamhyunjun.ostargram.domain.post.service.PostService;
+import shop.iamhyunjun.ostargram.security.customfilter.UserDetailsImpl;
+
+import java.util.List;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/posts")
 public class PostController {
+
+    private final PostService postService;
+
+    //글 목록 조회
+    @GetMapping
+    public Result postList() {
+        List<PostResponseDto> postList = postService.findPosts();
+        return new Result(postList);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T postList;
+    }
+
+    //글 작성
+    @PostMapping("/write")
+    public String write(@Validated @ModelAttribute PostSaveDto postSaveDto) {
+        postService.save(postSaveDto);
+        return "posted";
+    }
+
+    //글 단건 조회
+    @GetMapping("/{postId}")
+    public PostPage seePost(@PathVariable Long postId) {
+        Post post = postService.findPost(postId);
+        return new PostPage(new PostResponseDto(post));
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class PostPage<T1> {
+        private T1 post;
+    }
+
+    //글 수정
+    @PostMapping("/{postId}/edit")
+    public String edit(@PathVariable Long postId,
+                       @Validated @ModelAttribute PostUpdateDto postUpdateDto,
+                       @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        postService.updatePost(postId, postUpdateDto,userDetails);
+        return "edited";
+    }
+
+    //글 삭제
+    @DeleteMapping("/{postId}")
+    public String delete(@PathVariable Long postId,
+                         @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        postService.delete(postId,userDetails);
+        return "deleted";
+    }
 }
--- src/main/java/shop/iamhyunjun/ostargram/domain/post/service/PostService.java
+++ src/main/java/shop/iamhyunjun/ostargram/domain/post/service/PostService.java
@@ -1,4 +1,71 @@
 package shop.iamhyunjun.ostargram.domain.post.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import shop.iamhyunjun.ostargram.domain.post.dto.PostResponseDto;
+import shop.iamhyunjun.ostargram.domain.post.dto.PostSaveDto;
+import shop.iamhyunjun.ostargram.domain.post.dto.PostUpdateDto;
+import shop.iamhyunjun.ostargram.domain.post.entity.Post;
+import shop.iamhyunjun.ostargram.domain.post.repositiory.PostRepository;
+import shop.iamhyunjun.ostargram.security.customfilter.UserDetailsImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class PostService {
+
+    private final PostRepository postRepository;
+
+    public List<PostResponseDto> findPosts() {
+        List<PostResponseDto> postList = new ArrayList<>();
+        List<Post> posts = postRepository.findAllByOrderByCreatedAtDesc();
+        for (Post post : posts) {
+            postList.add(new PostResponseDto(post));
+        }
+        return postList;
+    }
+
+    public Post findPost(Long postId) {
+        Post foundPost = postRepository.findById(postId).orElseThrow(
+                () -> new IllegalArgumentException("잘못된 요청입니다.")
+        );
+        return foundPost;
+    }
+
+    @Transactional
+    public Post save(PostSaveDto postSaveDto) {
+        Post savePost = new Post(postSaveDto);
+        postRepository.save(savePost);
+        return savePost;
+    }
+
+    @Transactional
+    public void updatePost(Long postId, PostUpdateDto postUpdateDto, UserDetailsImpl userDetails) {
+        Post foundPost = postRepository.findById(postId).orElseThrow(
+                () -> new IllegalArgumentException("잘못된 요청입니다.")
+        );
+        if (userDetails.getUser().getId().equals(foundPost.getUser().getId())) {
+            foundPost.update(postUpdateDto);
+        } else throw new IllegalAccessError();
+    }
+
+    @Transactional
+    public void delete(Long postId,UserDetailsImpl userDetails) {
+
+        Post foundPost = postRepository.findById(postId).orElseThrow(
+                () -> new IllegalArgumentException("잘못된 요청입니다.")
+        );
+        if (userDetails.getUser().getId().equals(foundPost.getUser().getId())){
+            postRepository.deleteById(postId);
+        } else throw new IllegalAccessError();
+
+    }
+
+
+
 }
