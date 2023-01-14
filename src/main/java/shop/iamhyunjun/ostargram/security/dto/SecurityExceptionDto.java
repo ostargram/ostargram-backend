8a038db8ff9f726fe169a355ecbaa2f09ac01dca
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-01-14 17:33:17 +0900

    로그인 실패, 권한 실패 exceptionHandling 추가

---
+++ src/main/java/shop/iamhyunjun/ostargram/security/dto/SecurityExceptionDto.java
@@ -0,0 +1,17 @@
+package shop.iamhyunjun.ostargram.security.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class SecurityExceptionDto {
+
+    private int statusCode;
+    private String msg;
+
+    public SecurityExceptionDto(int statusCode, String msg) {
+        this.statusCode = statusCode;
+        this.msg = msg;
+    }
+}
