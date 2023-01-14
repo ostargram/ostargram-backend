03789d2ec6bdb23d53bea044b5a7de3cfa970e64
midcondria <hyukkind@naver.com>
2023-01-14 20:28:07 +0900

    글작성 ver2

--- src/main/java/shop/iamhyunjun/ostargram/domain/post/dto/PostUpdateDto.java
+++ src/main/java/shop/iamhyunjun/ostargram/domain/post/dto/PostUpdateDto.java
@@ -3,13 +3,9 @@ package shop.iamhyunjun.ostargram.domain.post.dto;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
 public class PostUpdateDto {
-
-    @NotNull
-    private String author;
 
     @NotBlank
     private String title;
