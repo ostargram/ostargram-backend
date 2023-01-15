60133fcdcf7a646cfaef20619f7b4ea03866642c
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-01-16 00:23:25 +0900

    User @Validated 추가, 컬럼 변경

--- src/main/java/shop/iamhyunjun/ostargram/domain/user/dto/UserSignupRequestDto.java
+++ src/main/java/shop/iamhyunjun/ostargram/domain/user/dto/UserSignupRequestDto.java
@@ -4,11 +4,26 @@ package shop.iamhyunjun.ostargram.domain.user.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Setter
 @Getter
 public class UserSignupRequestDto {
+
+    @NotBlank
+    @Pattern(regexp = "(?=.*[a-z])(?=.*[0-9]).{4,10}$",
+            message = "아이디 형식이 올바르지 않습니다")
     private String username;
+
+    @NotBlank
+    @Pattern(regexp = "(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])^.{8,15}$",
+            message = "패스워드 형식이 올바르지 않습니다.")
     private String password;
-    private boolean admin = false;
-    private String adminToken = "";
+
+    private String checkPassword;
+
+    private String email;
+
+    private String gender;
 }
