390d6d3281a1ac49d42026b1576b374f72a3a5b6
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-01-19 23:15:38 +0900

    script 변경

--- deploy.sh
+++ deploy.sh
@@ -28,4 +28,4 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 # -Duser.timezone=Asia/Seoul
-nohup java -jar $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
